ea0b1eb56d77ebc8c5d67bf7a85a03da905d64b8
alexsev97 <alsevill@ucm.es>
2018-10-17 19:53:06 +0100

    Initial Commit

---
+++ Sevilla_CalculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sevilla_CalculatorApp
+//
+//  Created by Alejandro Sevilla Romero  on 17/10/2018.
+//  Copyright © 2018 Alejandro Sevilla Romero . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Sevilla_CalculatorAppTests/Sevilla_CalculatorAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sevilla_CalculatorAppTests.swift
+//  Sevilla_CalculatorAppTests
+//
+//  Created by Alejandro Sevilla Romero  on 17/10/2018.
+//  Copyright © 2018 Alejandro Sevilla Romero . All rights reserved.
+//
+
+import XCTest
+@testable import Sevilla_CalculatorApp
+
+class Sevilla_CalculatorAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
