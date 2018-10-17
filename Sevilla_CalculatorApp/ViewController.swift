22aecfae5948130093e3b5be9d87a4f9545afa1f
alexsev97 <alsevill@ucm.es>
2018-10-17 20:09:39 +0100

    First version finished

--- Sevilla_CalculatorApp/ViewController.swift
+++ Sevilla_CalculatorApp/ViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var A = 0
+var B = 0
+var C = 0
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func oneButton(_ sender: Any) {
+        A = 1
+    }
+    
+    
+    @IBAction func twoButton(_ sender: Any) {
+        B = 2
+    }
+    
+    
+    @IBAction func plusButton(_ sender: Any) {
+        C = A + B
+    }
+    
+    @IBAction func equalButton(_ sender: Any) {
+        resultLabel.text = String(C)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
